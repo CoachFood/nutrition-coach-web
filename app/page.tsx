5a10d9f0a329ff82ad48cfb2ae83c99b16561c14
CoachFood <nicolas.pierzo@gmail.com>
2025-12-28 09:26:43 +0100

    Create Home component for main page

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>CoachFood</h1>
+      <p>Application de conseil nutritionnel personnalisé</p>
+    </main>
+  );
+}
